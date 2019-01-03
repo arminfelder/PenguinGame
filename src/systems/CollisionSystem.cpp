16a4713b4839991f4877f2b29a8cfb56aa22c663
Mario Löscher <mario@loescher.tirol>
2019-01-03 15:35:53 +0100

    skip entity-collision if mask-collision found

--- src/systems/CollisionSystem.cpp
+++ src/systems/CollisionSystem.cpp
@@ -27,6 +27,7 @@ Systems::CollisionSystem::CollisionSystem(Managers::EventsManager *pEventsmanage
 
         //maskCollision
 
+        bool maskCollision = false;
         int maskRightLimit = (int) ceil((double)rightLimit/50.0);
         int maskLeftLimit = (int) ceil((double)leftLimit/50.0);
         int maskTopLimit = (int) ceil((double)topLimit/50.0);
@@ -36,6 +37,7 @@ Systems::CollisionSystem::CollisionSystem(Managers::EventsManager *pEventsmanage
             for (int vertical = maskTopLimit-1; vertical < maskBottomLimit; vertical++) {
                 if ((system->collisionMask->at(static_cast<unsigned long>(horizontal + vertical * system->mapWidth))) == true) {
                     std::cout << "collision via mask detected" << std::endl;
+                    maskCollision = true;
                     system->mEventsManager->addEvent(
                             std::make_shared<Events::CollisionEvent>(entityId, 0, Events::collisionTypes::regular));
                 }
@@ -44,60 +46,61 @@ Systems::CollisionSystem::CollisionSystem(Managers::EventsManager *pEventsmanage
 
         //end maskCollision
 
+        if (!maskCollision) {
+            for (const auto &entry: collideAbles) {
+                auto entrySpatial = Managers::ComponentsManager::getSpatialComponent(entry.first);
+                auto entryVisual = Managers::ComponentsManager::getVisualComponent(entry.first);
+                auto entryEntity = Managers::EntityManager::getEntity(entry.first);
+                int entryRightLimit = entryVisual->mImageRect.w + entrySpatial->mPositionX;
+                int entryLeftLimit = entrySpatial->mPositionX;
+                int entryTopLimit = entrySpatial->mPositionY;
+                int entryBottomLimit = entrySpatial->mPositionY + entryVisual->mImageRect.h;
 
-        for(const auto &entry: collideAbles){
-            auto entrySpatial = Managers::ComponentsManager::getSpatialComponent(entry.first);
-            auto entryVisual = Managers::ComponentsManager::getVisualComponent(entry.first);
-            auto entryEntity = Managers::EntityManager::getEntity(entry.first);
-            int entryRightLimit = entryVisual->mImageRect.w + entrySpatial->mPositionX;
-            int entryLeftLimit = entrySpatial->mPositionX;
-            int entryTopLimit = entrySpatial->mPositionY;
-            int entryBottomLimit = entrySpatial->mPositionY + entryVisual->mImageRect.h;
+                Events::collisionTypes collisionType;
+                switch (entryEntity->getType()) {
+                    case Entities::entityTypes::wall: {
+                        collisionType = Events::collisionTypes::regular;
+                        break;
+                    }
+                    case Entities::entityTypes::ladder: {
+                        collisionType = Events::collisionTypes::ladder;
+                        break;
+                    }
+                    case Entities::entityTypes::npc: {
+                        break;
+                    }
+                    case Entities::entityTypes::movementReset: {
+                        collisionType = Events::collisionTypes::movementReset;
+                        break;
+                    }
+                    case Entities::entityTypes::ladderEnd: {
+                        collisionType = Events::collisionTypes::ladderEnd;
+                        break;
+                    }
+                    case Entities::entityTypes::ladderBegin: {
+                        collisionType = Events::collisionTypes::ladderBegin;
+                    }
+                    case Entities::entityTypes::none: {
 
-            Events::collisionTypes collisionType;
-            switch (entryEntity->getType()){
-                case Entities::entityTypes::wall:{
-                    collisionType = Events::collisionTypes::regular;
-                    break;
+                    }
                 }
-                case Entities::entityTypes::ladder:{
-                    collisionType = Events::collisionTypes::ladder;
-                    break;
-                }
-                case Entities::entityTypes::npc:{
-                    break;
-                }
-                case Entities::entityTypes::movementReset:{
-                    collisionType = Events::collisionTypes::movementReset;
-                    break;
-                }
-                case Entities::entityTypes::ladderEnd:{
-                    collisionType = Events::collisionTypes::ladderEnd;
-                    break;
-                }
-                case Entities::entityTypes::ladderBegin:{
-                    collisionType = Events::collisionTypes::ladderBegin;
-                }
-                case Entities::entityTypes::none:{
 
-                }
-            }
-
-            if(leftLimit>entryLeftLimit && leftLimit<entryRightLimit){
-                if(topLimit>entryTopLimit && topLimit < entryBottomLimit){
-                    system->mEventsManager->addEvent(std::make_shared<Events::CollisionEvent>(entityId, entry.first, collisionType));
-                    std::cout<<"collision!"<<std::endl;
-                }else if(bottomLimit<entryBottomLimit && bottomLimit>entryTopLimit){
-                    system->mEventsManager->addEvent(std::make_shared<Events::CollisionEvent>(entityId, entry.first, collisionType));
-                    std::cout<<"collision!"<<std::endl;
-                }
-            }else if(rightLimit<entryRightLimit && rightLimit>entryLeftLimit ){
-                if(topLimit>entryTopLimit && topLimit < entryBottomLimit){
-                    system->mEventsManager->addEvent(std::make_shared<Events::CollisionEvent>(entityId, entry.first, collisionType));
-                    std::cout<<"collision!"<<std::endl;
-                }else if(bottomLimit< entryBottomLimit && bottomLimit>entryTopLimit){
-                    system->mEventsManager->addEvent(std::make_shared<Events::CollisionEvent>(entityId, entry.first, collisionType));
-                    std::cout<<"collision!"<<std::endl;
+                if (leftLimit > entryLeftLimit && leftLimit < entryRightLimit) {
+                    if (topLimit > entryTopLimit && topLimit < entryBottomLimit) {
+                        system->mEventsManager->addEvent(std::make_shared<Events::CollisionEvent>(entityId, entry.first, collisionType));
+                        std::cout << "collision!" << std::endl;
+                    } else if (bottomLimit < entryBottomLimit && bottomLimit > entryTopLimit) {
+                        system->mEventsManager->addEvent(std::make_shared<Events::CollisionEvent>(entityId, entry.first, collisionType));
+                        std::cout << "collision!" << std::endl;
+                    }
+                } else if (rightLimit < entryRightLimit && rightLimit > entryLeftLimit) {
+                    if (topLimit > entryTopLimit && topLimit < entryBottomLimit) {
+                        system->mEventsManager->addEvent(std::make_shared<Events::CollisionEvent>(entityId, entry.first, collisionType));
+                        std::cout << "collision!" << std::endl;
+                    } else if (bottomLimit < entryBottomLimit && bottomLimit > entryTopLimit) {
+                        system->mEventsManager->addEvent(std::make_shared<Events::CollisionEvent>(entityId, entry.first, collisionType));
+                        std::cout << "collision!" << std::endl;
+                    }
                 }
             }
         }
