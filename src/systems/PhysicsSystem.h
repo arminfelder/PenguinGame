0d708c0af9290decf5dfebb387c267d533fc3fff
Armin Felder <armin.felder@osalliance.com>
2019-01-10 21:44:20 +0100

    asdds

--- src/systems/PhysicsSystem.h
+++ src/systems/PhysicsSystem.h
@@ -5,7 +5,6 @@
 #ifndef GAME_PHYSICSSYSTEM_H
 #define GAME_PHYSICSSYSTEM_H
 
-#include <tiff.h>
 #include "System.h"
 #include "../managers/EventsManager.h"
 
@@ -13,7 +12,7 @@ namespace Systems {
     class PhysicsSystem {
     public:
         PhysicsSystem(Managers::EventsManager *pEventsManager);
-        void update(uint64);
+        void update(uint64_t);
 
     private:
         Managers::EventsManager *mEventsManager;
