20a4ca9ab9e94cf357602d5e3bcdc41c3a109168
Armin Felder <armin.felder@osalliance.com>
2019-01-12 14:52:56 +0100

    adds GPLv3 header

--- src/entities/Ladder.h
+++ src/entities/Ladder.h
@@ -1,3 +1,21 @@
+/******************************************************************************
+* Copyright (C) Mario Löscher, Armin Felder
+* This file is part of PenguinGame <https://git.felder-edv.at/uibkcpp18/game>.
+*
+* PenguinGame is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* PenguinGame is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with PenguinGame. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #ifndef GAME_LADDER_H
 #define GAME_LADDER_H
 
--- src/managers/EntityManager.h
+++ src/managers/EntityManager.h
@@ -1,6 +1,21 @@
-//
-// Created by armin on 14.12.18.
-//
+/******************************************************************************
+* Copyright (C) Mario Löscher, Armin Felder
+* This file is part of PenguinGame <https://git.felder-edv.at/uibkcpp18/game>.
+*
+* PenguinGame is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* PenguinGame is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with PenguinGame. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 
 #ifndef GAME_ENTITYMANAGER_H
 #define GAME_ENTITYMANAGER_H
