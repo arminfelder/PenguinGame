e0ae56ca396165dc7509235f262e1c9bc4900c64
Armin Felder <armin.felder@osalliance.com>
2019-02-12 17:19:33 +0100

    adds melee atack to enemies

---
+++ src/components/EndsGame.cpp
@@ -0,0 +1,5 @@
+//
+// Created by armin on 12.02.19.
+//
+
+#include "EndsGame.h"
---
+++ src/components/EndsGame.h
@@ -0,0 +1,14 @@
+//
+// Created by armin on 12.02.19.
+//
+
+#ifndef GAME_ENDSGAME_H
+#define GAME_ENDSGAME_H
+
+
+class EndsGame {
+
+};
+
+
+#endif //GAME_ENDSGAME_H
