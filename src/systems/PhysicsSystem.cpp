09ae902a1fe40a5b4fdf3a3926708ee43adee86b
Armin Felder <armin.felder@osalliance.com>
2019-02-12 22:13:35 +0100

    asd

--- src/systems/PhysicsSystem.cpp
+++ src/systems/PhysicsSystem.cpp
@@ -58,8 +58,6 @@ Systems::PhysicsSystem::PhysicsSystem(Managers::EventsManager *pEventsManager):m
 //                std::cout << "added event for falling after jumping" << std::endl;
             }
 
-            auto test = event->mKeys[SDL_SCANCODE_SPACE];
-
             if(event->mKeys[SDL_SCANCODE_LEFT]){
                 momentum->speedX = -6;
             }else if(event->mKeys[SDL_SCANCODE_RIGHT]){
