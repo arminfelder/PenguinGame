8eac5e13f0c3d6af76bbfcd9b161b1f79186717e
Armin Felder <armin.felder@osalliance.com>
2019-02-14 21:26:00 +0100

    bla

---
+++ src/menu/MapRoom.cpp
@@ -0,0 +1,28 @@
+//
+// Created by armin on 14.02.19.
+//
+
+#include <memory>
+#include "MapRoom.h"
+
+MapRoom::MapRoom(SDL_Rect pRect,const std::string &pColor):mRect(pRect),color(pColor) {
+
+}
+
+const SDL_Color MapRoom::matchColor(std::string color) {
+    SDL_Color sdl_color = {0, 0, 0, 0};
+    if (color == "grey")
+        sdl_color = {62, 62, 62, 255};
+    else if (color == "red")
+        sdl_color = {232,23,23, 255};
+    else if (color == "green")
+        sdl_color = {23,232,23, 128};
+    else if (color == "blue")
+        sdl_color = {23,23,232, 255};
+
+    return sdl_color;
+}
+
+std::shared_ptr<SDL_Surface> MapRoom::getSurface() {
+    return  std::shared_ptr<SDL_Surface>(SDL_CreateRGBSurface(0, mRect.h, mRect.h ,32,0,0,0,0), SDL_FreeSurface);;
+}
