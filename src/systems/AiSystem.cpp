cc478ba0ba59ebd4beec150c1b2bd1e25247085e
Armin Felder <armin.felder@osalliance.com>
2019-02-14 18:30:53 +0100

    friendly fire off

--- src/systems/AiSystem.cpp
+++ src/systems/AiSystem.cpp
@@ -14,51 +14,52 @@ Systems::AiSystem::AiSystem(Managers::EventsManager *pEventsManager):mEventsMana
 void Systems::AiSystem::update(uint8_t pTimeDiff) {
 
     for(const auto &path: Managers::ComponentsManager::getPaths()){
+        if(path.second) {
+            int diffX = path.second->mPath[path.second->mCurrPos].x - path.second->mXmovedCurStep;
+            int diffY = path.second->mPath[path.second->mCurrPos].y - path.second->mYmovedCurStep;
 
-        int diffX = path.second->mPath[path.second->mCurrPos].x - path.second->mXmovedCurStep;
-        int diffY = path.second->mPath[path.second->mCurrPos].y - path.second->mYmovedCurStep;
+            path.second->mTimeSinceLastStep += pTimeDiff;
 
-        path.second->mTimeSinceLastStep += pTimeDiff;
-
-        if(path.second->mRunning && !path.second->mPaused){
-            int moveX = 0;
-            int moveY = 0;
-            if(!diffX &&!diffY){
-                if(path.second->mCurrPos < static_cast<int>(path.second->mPath.size()-1)){
-                    path.second->mCurrPos++;
-                    path.second->mXmovedCurStep = 0;
-                    path.second->mYmovedCurStep = 0;
-                    moveX = path.second->mPath[path.second->mCurrPos].x - path.second->mXmovedCurStep;
-                    moveY = path.second->mPath[path.second->mCurrPos].y - path.second->mYmovedCurStep;
-                }else if(path.second->mRepeat){
-                    path.second->mCurrPos = 0;
-                    path.second->mXmovedCurStep = 0;
-                    path.second->mYmovedCurStep = 0;
-                    moveX = path.second->mPath[path.second->mCurrPos].x - path.second->mXmovedCurStep;
-                    moveY = path.second->mPath[path.second->mCurrPos].y - path.second->mYmovedCurStep;
-                } else{
-                    break;
+            if (path.second->mRunning && !path.second->mPaused) {
+                int moveX = 0;
+                int moveY = 0;
+                if (!diffX && !diffY) {
+                    if (path.second->mCurrPos < static_cast<int>(path.second->mPath.size() - 1)) {
+                        path.second->mCurrPos++;
+                        path.second->mXmovedCurStep = 0;
+                        path.second->mYmovedCurStep = 0;
+                        moveX = path.second->mPath[path.second->mCurrPos].x - path.second->mXmovedCurStep;
+                        moveY = path.second->mPath[path.second->mCurrPos].y - path.second->mYmovedCurStep;
+                    } else if (path.second->mRepeat) {
+                        path.second->mCurrPos = 0;
+                        path.second->mXmovedCurStep = 0;
+                        path.second->mYmovedCurStep = 0;
+                        moveX = path.second->mPath[path.second->mCurrPos].x - path.second->mXmovedCurStep;
+                        moveY = path.second->mPath[path.second->mCurrPos].y - path.second->mYmovedCurStep;
+                    } else {
+                        break;
+                    }
+                } else {
+                    moveX = diffX;
+                    moveY = diffY;
                 }
-            }else{
-                moveX = diffX;
-                moveY = diffY;
-            }
 
-            if(diffX<0){
-                moveX = std::clamp(moveX,-path.second->mStepsPerFrame,0);
-            }else{
-                moveX = std::clamp(moveX,0,path.second->mStepsPerFrame);
-            }
-            if(diffY<0){
-                moveY = std::clamp(moveY,-path.second->mStepsPerFrame,0);
-            }else{
-                moveY = std::clamp(moveY,0,path.second->mStepsPerFrame);
-            }
-            path.second->mXmovedCurStep += moveX;
-            path.second->mYmovedCurStep += moveY;
+                if (diffX < 0) {
+                    moveX = std::clamp(moveX, -path.second->mStepsPerFrame, 0);
+                } else {
+                    moveX = std::clamp(moveX, 0, path.second->mStepsPerFrame);
+                }
+                if (diffY < 0) {
+                    moveY = std::clamp(moveY, -path.second->mStepsPerFrame, 0);
+                } else {
+                    moveY = std::clamp(moveY, 0, path.second->mStepsPerFrame);
+                }
+                path.second->mXmovedCurStep += moveX;
+                path.second->mYmovedCurStep += moveY;
 
-            mEventsManager->addEvent(std::make_shared<Events::MoveEntity>(path.first,moveX,moveY ));
+                mEventsManager->addEvent(std::make_shared<Events::MoveEntity>(path.first, moveX, moveY));
 
+            }
         }
     }
 
@@ -100,7 +101,9 @@ if(time>=1000) {
                 mEventsManager->addEvent(std::make_shared<Events::EntityCanSee>(viewRange.first, 1));
             } else {
                 auto path = Managers::ComponentsManager::getPaths(viewRange.first);
-                path->mPaused = false;
+                if (path) {
+                    path->mPaused = false;
+                }
             }
         }
     }
